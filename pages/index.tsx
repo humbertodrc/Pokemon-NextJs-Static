73698eea5bfd44f645c57942a565002fe2da05fa
danieldamian09 <humbertorivero9@gmail.com>
2022-05-11 16:45:39 -0300

    Add NextUI - React UI Library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
 
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>Hola Humberto</h1>
+      <Button color={'gradient'}>Soy Button</Button>
     </>
   )
 }
