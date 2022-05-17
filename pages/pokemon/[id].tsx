77432d28aa6a43b9494733b76fbe2ca7059e78b6
danieldamian09 <humbertorivero9@gmail.com>
2022-05-17 20:28:03 -0300

    Fix Card Header button favorites

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -61,7 +61,7 @@ const PokemonPage: NextPage<Props> = ({pokemon}) => {
 				<Grid xs={12} sm={8}>
 					<Card>
 						<Card.Header
-							css={{display: "flex", justifyContent: "space-between"}}
+							css={{display: "flex", justifyContent: "space-between", flexWrap:"wrap", gap: "10px"}}
 						>
 							<Text h1 transform="capitalize">
 								{pokemon.name}
